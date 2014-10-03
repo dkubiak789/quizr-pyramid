6b8c8dff84f3d5018d372268b5ecd6e5346a6c99
Wojciech Lichota <wojciech@lichota.pl>
2014-10-03 21:41:02 +0200

    Poprawki pep8

--- setup.py
+++ setup.py
@@ -21,16 +21,17 @@ requires = [
     'waitress',
     'pycrypto',
     'webtest',
-    ]
+]
 
-if sys.version_info[:3] < (2,5,0):
+if sys.version_info[:3] < (2, 5, 0):
     raise RuntimeError('This application requires Python 2.6+')
 
-setup(name='quizr',
-      version='0.1.0',
-      description='A quiz application (in Pyramid).',
-      long_description=README,
-      classifiers=[
+setup(
+    name='quizr',
+    version='0.1.0',
+    description='A quiz application (in Pyramid).',
+    long_description=README,
+    classifiers=[
         "Framework :: Pyramid",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
@@ -38,22 +39,21 @@ setup(name='quizr',
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Internet :: WWW/HTTP :: WSGI",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author="Wojciech Lichota",
-      author_email="wojciech@lichota.pl",
-      url='https://github.com/sargo/quizr-pyramid',
-      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
-      keywords='web wsgi pyramid quiz quizr',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      test_suite='quizr.tests',
-      install_requires=requires,
-      entry_points="""\
-      [paste.app_factory]
-      main = quizr:main
-      [console_scripts]
-      initialize_quizr_db = quizr.scripts.initializedb:main
-      """,
-      )
-
+    ],
+    author="Wojciech Lichota",
+    author_email="wojciech@lichota.pl",
+    url='https://github.com/sargo/quizr-pyramid',
+    license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+    keywords='web wsgi pyramid quiz quizr',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='quizr.tests',
+    install_requires=requires,
+    entry_points="""\
+    [paste.app_factory]
+    main = quizr:main
+    [console_scripts]
+    initialize_quizr_db = quizr.scripts.initializedb:main
+    """,
+)
