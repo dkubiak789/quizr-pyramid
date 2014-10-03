c6486dc6bac90d89490c4121b24736432b91b1bb
Wojciech Lichota <wojciech@lichota.pl>
2014-10-03 21:31:59 +0200

    Baza w katalogu var

--- quizr/scripts/initializedb.py
+++ quizr/scripts/initializedb.py
@@ -6,12 +6,12 @@ from sqlalchemy import engine_from_config
 from pyramid.paster import (
     get_appsettings,
     setup_logging,
-    )
+)
 
 from quizr.models import (
     DBSession,
     Base,
-    )
+)
 
 def usage(argv):
     cmd = os.path.basename(argv[0])
@@ -26,5 +26,8 @@ def main(argv=sys.argv):
     setup_logging(config_uri)
     settings = get_appsettings(config_uri)
     engine = engine_from_config(settings, 'sqlalchemy.')
+    db_dir = os.path.dirname(engine.url.database)
+    if not os.path.exists(db_dir):
+        os.makedirs(db_dir)
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
